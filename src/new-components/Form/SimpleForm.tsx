2249a0bb2e353d942fb57c97505895531ce9bc99
Abhijeet Singh Khangarot <abhijeet40308@gmail.com>
2023-02-15 08:22:31 +0530

    remove unnecessary files

--- src/new-components/Form/SimpleForm.tsx
+++ src/new-components/Form/SimpleForm.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import { v4 as uuid } from 'uuid';
-import { infer as zodInfer } from 'zod';
 import { useConsoleForm } from './hooks/useConsoleForm';
-import { FormProps, UseConsoleFormProps, Schema } from './hooks/form.types';
+import { Schema } from './hooks/form.types';
 
-export const SimpleForm = <FormSchema extends Schema>(
-  props: FormProps<zodInfer<FormSchema>> &
-    UseConsoleFormProps<zodInfer<FormSchema>, FormSchema>
-) => {
+export const SimpleForm = <FormSchema extends Schema>(props: any) => {
   const { Form } = useConsoleForm<FormSchema>(props);
   return <Form {...props} />;
 };
